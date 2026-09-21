5791a76d5f1ce0797fbd0fbbeb56cc9693c6d046
Blobes <officialblobes@gmail.com>
2026-09-21 12:04:03 +0100

    Fixed

--- frontend-coolify/packages/features/src/apps/post/components/header/Author.tsx
+++ frontend-coolify/packages/features/src/apps/post/components/header/Author.tsx
@@ -24,9 +24,11 @@ export const AuthorInfo = ({
   return (
     <Stack
       direction="row"
-      sx={{ alignItems: "center", width: "100%", gap: theme.gap(2), ...style }}>
+      sx={{ alignItems: "center", width: "100%", gap: theme.gap(2), ...style }}
+    >
       <UserAvatar
         userInfo={{
+          _id: author._id,
           firstName: author.firstName,
           lastName: author.lastName,
           profileImage: author.profileImage,
@@ -37,7 +39,8 @@ export const AuthorInfo = ({
       <Stack sx={{ width: "100%", gap: theme.gap(0), minWidth: "40px" }}>
         <TransText
           noWrap
-          sx={{ ...theme.typography.text3, fontWeight: "bold" }}>
+          sx={{ ...theme.typography.text3, fontWeight: "bold" }}
+        >
           {author.fullName}
         </TransText>
         {showUsername && (
@@ -47,7 +50,8 @@ export const AuthorInfo = ({
               ...theme.typography.text4,
               color: theme.palette.gray[200],
               lineHeight: "1.1em",
-            }}>
+            }}
+          >
             @{author.username}
           </TransText>
         )}
